4348f753224f6384e91b228328b67805a9706d34
Gilgalad195 <gilgalad195@gmail.com>
2025-04-16 16:10:19 -0500

    working sort and filter

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def get_book_text (filepath):
         file_contents = book.read()
         return file_contents
     
-
      
 
 def main():
@@ -14,7 +13,18 @@ def main():
     word_count = get_word_count(contents)
     count_dict = count_characters(contents)
     print (f"{word_count} words found in the document")
-    print (count_dict)
+    #print (count_dict)
+
+    chars_list = []
+    def sort_on(dict):
+        return dict["count"]
+    for char, count in count_dict.items():
+        if char.isalpha():
+            chars_list.append({"char": char, "count": count})
+            chars_list.sort(reverse=True, key=sort_on)
+    print(chars_list)
+
+    #NOW I NEED TO GET THE DICTIONARY LIST BACK INTO A SINGLE KEY:VALUE PAIR DICTIONARY
 
 
 main()
